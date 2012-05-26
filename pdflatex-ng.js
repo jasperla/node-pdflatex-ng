36aacf6c0bfec7b4fd197c716d00ece1da3918f6
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2012-05-26 20:40:12 +0200

    Add some input validation.

--- pdflatex-ng.js
+++ pdflatex-ng.js
@@ -1,29 +1,62 @@
 var exec = require('child_process').exec
   ,   fs = require('fs')
+, mkdirp = require('mkdirp')
   , path = require('path')
   , util = require('util');
 
+/* Constructor providing some defaults. */
 function PDFLatex(inputPath) {
 	this.outputDirectory = process.cwd() + "/";
 	this.inputPath = inputPath;
 };
 
-PDFLatex.prototype.outputDir = function(path) {
+/* Allow changing the output directory. */
+PDFLatex.prototype.setOutputDir = function(path) {
 	this.outputDirectory = path;
 	return this;
 };
 
-PDFLatex.prototype.process = function() {
-//	if (validateInput(this.inputPath)) {
-	if (this.inputPath && this.inputPath.length > 0) {
-		var command = "pdflatex -output-directory " +
-		    this.outputDirectory + " '" + this.inputPath + "'";
-		util.puts(command);
-		exec(command, function(err) {
-			if (err) throw err;
+validateInput = function(input) {
+	if (input.length > 0) {
+		path.exists(input, function(exists) {
+			if (!exists) {
+				throw new Error("Invalid input provided. Does the file exist?");
+			}
 		});
+	}
+	return true;
+};
+
+validOutputDir = function(dir) {
+	var outputDir = dir;
+	fs.lstat(outputDir, function(err, stats) {
+			if (!err && stats.isDirectory()) {
+				return true;
+			} else if (!err && stats.isFile()){
+				throw new Error("Cannot write to " + outputDir + ", it is a file");
+				return false;
+			} else {
+				mkdirp(outputDir, function(err) {
+						if (err) console.error(err);
+				});
+				return true;
+			}
+	});
+	return true;
+};
+
+PDFLatex.prototype.typeset = function() {
+	if (validOutputDir(this.outputDirectory)) {
+		if (validateInput(this.inputPath)) {
+			var command = "pdflatex -output-directory " +
+			    this.outputDirectory + " '" + this.inputPath + "'";
+			util.puts(command);
+			exec(command, function(err) {
+				if (err) throw err;
+			});
+		}
 	} else {
-		throw new Error("Invalid input provided. Does the file exist?");
+		return false;
 	}
 };
 
